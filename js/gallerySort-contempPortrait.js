25a35a558bad5310a419e1c7cc38a6342c451aab
backlightbabe <polly@portrayals-photography.com>
2023-03-28 21:26:16 -0700

    still working on js

--- js/gallerySort-contempPortrait.js
+++ js/gallerySort-contempPortrait.js
@@ -11,10 +11,32 @@ document.querySelector("#contempPortrait").addEventListener("click", showContemp
 
 function showContempPortrait() {
   window.location.href= "gallery.html";
-  document.getElementByID("#contempPortBtn").click();
-  
-}
 
-// contempPortBtn is id for menu item that sorts for contemp portraits
-//
-//
+  // [ Portfolio items & filtering ]
+  // */
+  jQuery(window).on("load", function(){
+    jQuery('.filtering-wrap').each(function(){
+      var $grid = jQuery(this).find('.items').isotope();
+
+      if($grid.hasClass('masonry')){
+        var $grid = jQuery(this).find('.items').isotope({
+          itemSelector: '.item',
+          masonry: {
+            columnWidth: '.item'
+          }
+        });
+      } else {
+        var $grid = jQuery(this).find('.items').isotope({
+          itemSelector: '.item'
+        });
+      }
+
+      jQuery(this).find('.filter-button-group').on( 'click', 'button', function() {
+        jQuery(this).addClass('active').siblings().removeClass('active');
+        var filterValue = jQuery(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+      });
+    });
+  });
+
+}
